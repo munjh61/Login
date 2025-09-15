81d6eef67cc633d48241f5988f0b59a71271128e
Joonho <junho9661@gmail.com>
2025-09-15 10:04:37 +0900

    jpa

---
+++ GameDataServer/src/main/java/org/ssafy/gamedataserver/dto/ResponseDTO.java
@@ -0,0 +1,48 @@
+package org.ssafy.gamedataserver.dto;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.Builder;
+import lombok.Getter;
+import org.springframework.http.HttpStatus;
+
+@Getter
+@JsonInclude(JsonInclude.Include.NON_NULL)
+public class ResponseDTO<T> {
+    private final HttpStatus status;
+    private final String message;
+    private final T data;
+    private final long timestamp;
+
+    @Builder
+    private ResponseDTO(HttpStatus status, String message, T data, long timestamp) {
+        this.status = status;
+        this.message = message;
+        this.data = data;
+        this.timestamp = timestamp;
+    }
+
+    public static <T> ResponseDTO<T> ok(T data) {
+        return ResponseDTO.<T>builder()
+                .status(HttpStatus.OK)
+                .data(data)
+                .timestamp(System.currentTimeMillis())
+                .build();
+    }
+
+    public static <T> ResponseDTO<T> ok(String message, T data) {
+        return ResponseDTO.<T>builder()
+                .status(HttpStatus.OK)
+                .message(message)
+                .data(data)
+                .timestamp(System.currentTimeMillis())
+                .build();
+    }
+
+    public static <T> ResponseDTO<T> fail(String message) {
+        return ResponseDTO.<T>builder()
+                .status(HttpStatus.BAD_REQUEST)
+                .message(message)
+                .timestamp(System.currentTimeMillis())
+                .build();
+    }
+}
--- README.md
+++ README.md
@@ -3,7 +3,13 @@
 ## 로그인 기능 매번 만들기 귀찮다.
 
 ```
-1. MyBatis 썼다(dao, xml구조). JPA 쓰고 싶으면 바꿔라
-2. Token 주는 방식이다. Session storage 쓰려고 했고, cookie 쓰려면 따로 처리해
-3. Oauth2 안 했다. 언젠간 해야지
+SSAFIT
+1. MyBatis 썼다(dao, xml구조)
+2. Token 주는 방식이다. Session storage 쓰려고 했고, cookie 쓰려면 따로 처리해야함
+3. Oauth2 안 했다.
+
+GameServer
+1. JPA 썼다.
+2. Token 주는 방식이다.
+3. Oauth2 안했다.
 ```
